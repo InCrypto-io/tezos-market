b138810b34859bbf20ac53dc1348fce9ffbdcd97
Delukin Svyatoslav <svyatoslav.d@blaize.tech>
2019-09-06 16:44:20 +0300

    remove draft for wallet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import WalletTest from "./WalletTest";
-import ConseilJS from "./ConseilJS";
 import WalletServiceTest from "./WalletServiceTest";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <WalletTest></WalletTest>
-          <ConseilJS></ConseilJS>
           <WalletServiceTest></WalletServiceTest>
       </header>
     </div>
