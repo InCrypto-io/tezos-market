dc285c72cc7ce619a714908b88363bb38a0db6fa
tarzeron <tarzeron@gmail.com>
2019-09-07 16:05:20 +0300

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -20,11 +20,15 @@ export default class Header extends React.Component {
     render() {
         const {pages, current} = this.state;
         return <div className={"header"}>
+            <img src="../logo.svg" alt=""/>
+            <span>title</span>
+            <div className={"header-links"}>
             {
                 pages.map(page => {
                     return <div className={page === current ? "active" : ""}><span onClick={()=>{this.setCurrent(page)}}>{page}</span></div>
                 })
             }
+        </div>
         </div>
     }
 };
