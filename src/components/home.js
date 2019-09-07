5ed606445309190f32370f72ff9747a55e8c9a64
tarzeron <tarzeron@gmail.com>
2019-09-07 16:12:31 +0300

    stub body

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,13 @@ import Header from "./header";
 export default class Home extends React.Component {
 
     render() {
-        return <div>
+        return <div className={"page-body"}>
             <Header></Header>
+            <span>Description</span>
+
+            <button>
+                Get started
+            </button>
         </div>
     }
 };
