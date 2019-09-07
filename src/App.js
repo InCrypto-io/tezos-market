dfb2f7f4670ce2a53dee4cd210130589e6ba7dde
mac <mac@macs-MacBook-Air.local>
2019-09-07 15:33:22 +0300

    add home page stub

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import WalletServiceTest from "./WalletServiceTest";
 import NFTTest from "./NFTTest";
+import Home from "./components/home"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <NFTTest></NFTTest>
-          <WalletServiceTest></WalletServiceTest>
+          {/* <NFTTest></NFTTest> */}
+          {/* <WalletServiceTest></WalletServiceTest> */}
+          <Home/>
       </header>
     </div>
   );
---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default class Home extends React.Component {
+
+    render() {
+        return <div>
+            <span>dsfsdfdsfds</span>
+        </div>
+    }
+};
