95bb819adfa3274dc2cdf222a56e84c0f0f435f7
tarzeron <tarzeron@gmail.com>
2019-09-08 14:43:55 +0300

    hard

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ export default class Footer extends React.Component {
     render() {
         return <div className={"footer"}>
             <img src={require("../footer.jpeg")} alt=""/>
+            <a className={"link"} href="blaize.tech">blaize.tech</a>
         </div>
     }
 };
